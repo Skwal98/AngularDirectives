7d3148dea749c86cd416589479c2b31b44d70175
Snowman <levkalex98@gmail.com>
2021-11-07 20:14:16 +0300

    prevented drag animation

--- src/app/components/models/dropped.model.ts
+++ src/app/components/models/dropped.model.ts
@@ -5,4 +5,14 @@ export class Dropped{
     constructor(
     readonly container: DropDirective<any>,
     readonly item?: DragDirective){}
+
+    private isPrevent: boolean = false; 
+
+    public preventDefault(){
+      this.isPrevent = true;
+    }
+
+    public get isPrevented(): boolean{
+      return this.isPrevent;
+    }
   }
